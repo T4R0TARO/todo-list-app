6094a2fabe75bb168d100316cd33e18f2940b54c
josh manansala <joshmanansalagsd@gmail.com>
2023-04-28 17:47:51 -0700

    basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,40 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log(count);
-
-  function countClick() {
-    setCount((prevCount) => prevCount + 1);
-  }
-
   return (
     <>
       <h1>TODO LIST APP</h1>
-      <button onClick={countClick}>Count</button>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input id="item" type="text" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-delete">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-delete">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-delete">Delete</button>
+        </li>
+      </ul>
     </>
   );
 }
