4690b948a461cda8316ddcc0c6e2c95026125b08
josh manansala <joshmanansalagsd@gmail.com>
2023-05-02 17:58:55 -0700

    add local storage feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import { nanoid } from "nanoid";
 import NewTodoForm from "./components/NewTodoForm";
 import TodoList from "./components/TodoList";
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  // get localStorage || return empty arr
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos")) || []
+  );
 
+  // add todo item obj
   function addTodo(title) {
     setTodos((prevState) => {
       return [...prevState, { id: nanoid(), title, completed: false }];
     });
   }
+
+  // set localStorage
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   // toggles checked props for todo items
   function toggleTodo(id, completed) {
@@ -61,13 +71,10 @@ function App() {
   return (
     <main>
       <h1>TODO LIST APP</h1>
-
       <NewTodoForm addTodo={addTodo} />
 
-      <h1 className="header">Todo List</h1>
-
       {/* Todo List Items */}
-
+      <h1 className="header">Todo List</h1>
       <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </main>
   );
